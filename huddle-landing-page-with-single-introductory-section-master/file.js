99e202ef061d3f0d6fe5f00bee0e3fa2f38ef001
kshitijSharma <43933347+kshitijsharma0506@users.noreply.github.com>
2020-06-24 15:25:02 +0530

    huddle-landing-page-with-single-introductory-section-master

---
+++ huddle-landing-page-with-single-introductory-section-master/file.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+    $('.modal').modal();
+    $('.sidenav').sidenav();
+    $('.slider').slider({full_width:true});
+    $('.parallax').parallax();
+    $('.mycarousel').carousel({
+        numVisible:7,
+        shift:55,
+        padding:55,
+    });
+});
